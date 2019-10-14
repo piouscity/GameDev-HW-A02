4aa5486a77177d45511ce0cd33d0c45352d12613
Trung-Hieu Ho <piouscity@outlook.com>
2019-10-14 21:49:02 +0700

    Clone from A01

---
+++ GameDev-HW/Assets/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float speed = 0.1f;
+    Animator animator;
+    Vector2 position;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        position = GetComponent<Transform>().position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool rightPressed = Input.GetKey(KeyCode.D);
+        bool leftPressed = Input.GetKey(KeyCode.A);
+        if (rightPressed || leftPressed)
+        {
+            if (rightPressed)
+            {
+                position.x += speed;
+                GetComponent<SpriteRenderer>().flipX = false;
+            }
+            else
+            {
+                position.x -= speed;
+                GetComponent<SpriteRenderer>().flipX = true;
+            }
+            GetComponent<Transform>().position = position;
+            animator.SetBool("moving", true);
+        }
+        else
+            animator.SetBool("moving", false);
+    }
+}
