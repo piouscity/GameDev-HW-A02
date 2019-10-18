489506b88fd251f77b8c7f1b3b12ef615b40f753
Trung-Hieu Ho <piouscity@outlook.com>
2019-10-18 21:49:56 +0700

    trigger animation jump and jump only twice

--- GameDev-HW/Assets/PlayerController.cs
+++ GameDev-HW/Assets/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     float speed = 0.1f;
     float jump_force = 600;
+    int jump_combo = 0;
     Animator animator;
     Vector2 position;
     void Start()
@@ -21,9 +22,11 @@ public class PlayerController : MonoBehaviour
         bool jumpPressed = Input.GetKeyDown(KeyCode.W);
         bool rightPressed = Input.GetKey(KeyCode.D);
         bool leftPressed = Input.GetKey(KeyCode.A);
-        if (jumpPressed)
+        if (jumpPressed && jump_combo < 2)
         {
             GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jump_force));
+            animator.SetBool("on_air", true);
+            ++jump_combo;
         }
         else if (rightPressed || leftPressed)
         {
@@ -43,5 +46,11 @@ public class PlayerController : MonoBehaviour
         }
         else
             animator.SetBool("moving", false);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        animator.SetBool("on_air", false);
+        jump_combo = 0;
     }
 }
